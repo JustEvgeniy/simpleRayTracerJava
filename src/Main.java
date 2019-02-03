c43beeee31403a6c0ff52ef23521a81c8e82b7d5
Evgeniy Vasilyev <ju5tc4t@gmail.com>
2019-02-04 01:18:29 +0300

    Step 1 done: displaying image in window

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,66 @@
-import Geometry.Vec2;
 import Geometry.Vec3;
 
-public class Main {
-    public static void main(String[] args) {
-        Vec2 vec2 = new Vec2(1., 2.);
-        System.out.println("vec2 = " + vec2);
-        System.out.println("vec2.length() = " + vec2.length());
-        System.out.println("vec2.normalize() = " + vec2.normalize());
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 
-        Vec3 vec3 = new Vec3(1., 2., 3.);
-        System.out.println("vec3 = " + vec3);
-        System.out.println("vec3.length() = " + vec3.length());
-        System.out.println("vec3.normalize() = " + vec3.normalize());
+public class Main extends Canvas {
+    public static void main(String[] args) {
+        render();
+    }
+
+    public static final int width = 1024;
+    public static final int height = 768;
+    public static Vec3[] frameBuffer;
+
+    private static void render() {
+        frameBuffer = new Vec3[width * height];
+
+        System.out.println("Created frameBuffer");
+
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                frameBuffer[j + i * height] = new Vec3((double) i / width, (double) j / height, 0.0);
+            }
+        }
+
+        System.out.println("Filled frameBuffer");
+
+        showImage();
+    }
+
+    private static void showImage() {
+        javax.swing.SwingUtilities.invokeLater(Main::createAndShowGUI);
+    }
+
+    private static void createAndShowGUI() {
+        JFrame frame = new JFrame("Image");
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(new Main());
+        frame.setSize(width, height);
+        frame.setResizable(false);
+        frame.setVisible(true);
+
+        System.out.println("Displaying image");
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                int color = 0;
+                color += 255 * frameBuffer[j + i * height].getX();
+                color <<= 8;
+                color += 255 * frameBuffer[j + i * height].getY();
+                color <<= 8;
+                color += 255 * frameBuffer[j + i * height].getZ();
+                image.setRGB(i, j, color);
+            }
+        }
+
+        g.drawImage(image, 0, 0, this);
     }
 }
